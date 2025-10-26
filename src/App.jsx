7e84482b69ed2d93d0029669434e3e5308eaf982
tuikobt <leminhphuong15032005@gmail.com>
2025-10-26 22:31:16 +0700

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/layout/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      {/* Add more components here */}
+      <h1 className="text-3xl font-bold text-red-500">Lê Minh Phương</h1>
+    </>
+  );
+}
+
+export default App;
